0f0a515809558fbd168ac43b26722dede8cf0a13
coopTRUE <coopdog4545@gmail.com>
2023-03-10 12:19:07 -0800

    initial files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+  <title>Home | ACHOS DeFi</title>
+  <meta
+    name="description"
+    content="Achos Defi is the most advanced DeFi consulting firm in the world. We are a team of experts in the DeFi space, with a focus on providing the best DeFi solutions to our clients."
+  />
+</svelte:head>
+
+<h1>Welcome to ACHOS DeFi</h1>
---
+++ src/routes/sign-in/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  import { page } from '$app/stores'
+  import { signIn, signOut } from '@auth/sveltekit/client'
+</script>
+
+<button on:click={() => signIn('google')}>Sign In</button>
+<code>{JSON.stringify($page.data)}</code>
